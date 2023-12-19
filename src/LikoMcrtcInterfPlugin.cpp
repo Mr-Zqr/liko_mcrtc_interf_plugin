9cef4b6f54c185446f74ae04c452ed3c677a927f
zhenyuanfu <zhenyuanfu18@gmail.com>
2023-12-19 16:56:47 +0800

    fu commit.

--- src/LikoMcrtcInterfPlugin.cpp
+++ src/LikoMcrtcInterfPlugin.cpp
@@ -12,20 +12,32 @@ void LikoMcrtcInterfPlugin::init(mc_control::MCGlobalController & controller, co
   mc_rtc::log::info("LikoMcrtcInterfPlugin::init called with configuration:\n{}", config.dump(true, true));
   // ros::NodeHandle this_nh;
   // nh_ = &this_nh;
+  bitbot_orientation_torso_ = Eigen::Quaterniond(0, 0, 0, 1);
+  bitbot_position_torso_ = Eigen::Vector3d(0, 0, 0);
   liko_sub_ = nh_->subscribe<nav_msgs::Odometry>("/bitbot_se", 1000, &LikoMcrtcInterfPlugin::liko_callback, this);
   controller.controller().datastore().make<Eigen::Quaterniond>("orientation_torso", bitbot_orientation_torso_);
   controller.controller().datastore().make<Eigen::Vector3d>("position_torso", bitbot_position_torso_);
   controller.controller().datastore().make<Eigen::Vector3d>("velocity_torso", bitbot_velocity_torso_);
 
   config("show_coordinate", disp_liko_coordinate_);
-
-  update_thread_ = std::thread([this]() {
-  ros::Rate rate(5000);
-  while(ros::ok())
+  if(disp_liko_coordinate_)
   {
-    ros::spinOnce();  
+    controller.controller().gui()->addElement(
+        {"LikoMcrtcInterfPlugin"},
+        mc_rtc::gui::Transform("Marker",
+                               [this]() {
+                                 return sva::PTransformd{bitbot_orientation_torso_.conjugate(), bitbot_position_torso_};
+                               }));
   }
-  });
+  update_thread_ = std::thread(
+      [this]()
+      {
+        ros::Rate rate(5000);
+        while(ros::ok())
+        {
+          ros::spinOnce();
+        }
+      });
 }
 
 void LikoMcrtcInterfPlugin::reset(mc_control::MCGlobalController & controller)
@@ -40,7 +52,6 @@ void LikoMcrtcInterfPlugin::reset(mc_control::MCGlobalController & controller)
 void LikoMcrtcInterfPlugin::before(mc_control::MCGlobalController & controller)
 {
   // mc_rtc::log::info("LikoMcrtcInterfPlugin::before");
-
 }
 
 void LikoMcrtcInterfPlugin::after(mc_control::MCGlobalController & controller)
@@ -50,12 +61,6 @@ void LikoMcrtcInterfPlugin::after(mc_control::MCGlobalController & controller)
   controller.controller().datastore().assign("orientation_torso", bitbot_orientation_torso_);
   controller.controller().datastore().assign("position_torso", bitbot_position_torso_);
   controller.controller().datastore().assign("velocity_torso", bitbot_velocity_torso_);
-  if(disp_liko_coordinate_)
-  {
-    controller.controller().gui()->addElement({"LikoMcrtcInterfPlugin"}, mc_rtc::gui::Transform("Marker", [this]() {
-        return sva::PTransformd{bitbot_orientation_torso_, bitbot_position_torso_};
-    }));    
-  }
 }
 
 mc_control::GlobalPlugin::GlobalPluginConfiguration LikoMcrtcInterfPlugin::configuration()
@@ -70,9 +75,12 @@ mc_control::GlobalPlugin::GlobalPluginConfiguration LikoMcrtcInterfPlugin::confi
 void LikoMcrtcInterfPlugin::liko_callback(const nav_msgs::Odometry::ConstPtr & msg)
 {
   std::unique_lock<std::mutex> lock(update_mutex_);
-  bitbot_position_torso_ = Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
-  bitbot_velocity_torso_ = Eigen::Vector3d(msg->twist.twist.linear.x, msg->twist.twist.linear.y, msg->twist.twist.linear.z);
-  bitbot_orientation_torso_ = Eigen::Quaterniond(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
+  bitbot_position_torso_ =
+      Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
+  bitbot_velocity_torso_ =
+      Eigen::Vector3d(msg->twist.twist.linear.x, msg->twist.twist.linear.y, msg->twist.twist.linear.z);
+  bitbot_orientation_torso_ = Eigen::Quaterniond(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x,
+                                                 msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
 }
 } // namespace mc_plugin
 
